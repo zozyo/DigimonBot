f328c163ebad6990423a66c9eb902b508040ed3b
zozyo <zzy.touhou@gmail.com>
2018-10-16 16:08:51 -0500

    Update addNumber.js

--- addNumber.js
+++ addNumber.js
@@ -1,6 +1,9 @@
 exports.add = function (a,b) {
-	if (a != null && b != null)
+	if (a != null && b != null) {
+		a = parseInt(a);
+		b = parseInt(b);
 		return a + b;
+	}
 	else
 		return 'Error!';
 }
