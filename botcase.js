0c6fd6e58848589b2c3a9dad9bc4c5eda916b028
zozyo <zzy.touhou@gmail.com>
2018-10-17 16:15:18 -0500

    Update botcase.js

--- botcase.js
+++ botcase.js
@@ -95,6 +95,7 @@ exports.cases = function (user, userID, channelID, message, evt, callback) {
 		case 'l':
 			db.l(function(players) {
 				console.log(players);
+				console.log(JSON.parse(players));
 				content = {
 					"color": 12345678, 
 					"title": "Players",
