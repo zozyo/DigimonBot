68c2d349c1851272ebe1f2e17ed162d94d4afde0
zozyo <zzy.touhou@gmail.com>
2018-10-17 07:28:12 -0500

    Update db.js

--- db.js
+++ db.js
@@ -23,6 +23,6 @@ MongoClient.connect(url, { useNewUrlParser: true }, function(err, database) {
 			if (err) throw err;
 			console.log(result);
 		});
-		return results;
+		return "results";
 	}; 
 });//end of db
