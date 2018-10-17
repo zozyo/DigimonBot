f42d51f7660d3aed6ecd8e1828b3a44a610a52c2
zozyo <zzy.touhou@gmail.com>
2018-10-17 04:41:27 -0500

    Update botcase.js

--- botcase.js
+++ botcase.js
@@ -32,7 +32,7 @@ exports.cases = function (user, userID, channelID, message, evt, callback) {
 			content = {
 				"color": 12345678,
 				"image": {
-					"url": "https://cdn.discordapp.com/embed/avatars/0.png"
+					"url": "./logo.jpg"
 				}
 			};
 		break;
