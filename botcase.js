64c829236ee255cc3a1b207a64f38863ebc672a1
zozyo <zzy.touhou@gmail.com>
2018-10-17 03:03:49 -0500

    Update botcase.js

--- botcase.js
+++ botcase.js
@@ -18,7 +18,53 @@ exports.cases = function (user, userID, channelID, message, evt, callback) {
 		break;
 		// !hello
 		case 'hello':
-			content = '"color": 12345678, "fields": [{ "value": "Hello world!" }]';
+			content = {"content": "this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```",
+  "embed": {
+    "title": "title ~~(did you know you can have markdown here too?)~~",
+    "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+    "url": "https://discordapp.com",
+    "color": 8405556,
+    "timestamp": "2018-10-17T06:33:05.924Z",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+      "text": "footer text"
+    },
+    "thumbnail": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "image": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "author": {
+      "name": "author name",
+      "url": "https://discordapp.com",
+      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "fields": [
+      {
+        "name": "🤔",
+        "value": "some of these properties have certain limits..."
+      },
+      {
+        "name": "😱",
+        "value": "try exceeding some of them!"
+      },
+      {
+        "name": "🙄",
+        "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "these last two",
+        "inline": true
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "are inline fields",
+        "inline": true
+      }
+    ]
+  };}
 		break;
 		// !logo
 		case 'logo':
