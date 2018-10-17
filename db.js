fc371eb3a946654fb70d7476b64a52a892c8bcea
zozyo <zzy.touhou@gmail.com>
2018-10-17 06:01:18 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, database) {
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, database) {
 
 	if (err) throw err;
 	var db = database.db("digimon");
