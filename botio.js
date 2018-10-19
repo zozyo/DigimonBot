a0556fcd0348538e51b4ccaafd9728890fc4273f
zozyo <zzy.touhou@gmail.com>
2018-10-19 02:27:21 -0500

    Update botio.js

--- botio.js
+++ botio.js
@@ -73,7 +73,7 @@ function splitMessage(message) {
 	const cmd = msg[0];
 	const args = msg.splice(1);
 
-	console.log(Cases.prototype);
+	console.log(Cases);
 	console.log(Cases.cmd);
 	// check the command exists
 	if (typeof Cases.cmd === 'function') {
