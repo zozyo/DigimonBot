1343b06472d409f01b56bfc14080af87b140146a
zozyo <zzy.touhou@gmail.com>
2018-10-19 01:44:43 -0500

    Update custom-loader.mjs

--- custom-loader.mjs
+++ custom-loader.mjs
@@ -1,36 +1,32 @@
-import url from 'url';
 import path from 'path';
 import process from 'process';
+import Module from 'module';
 
-const builtins = new Set(
-  Object.keys(process.binding('natives')).filter((str) =>
-    /^(?!(?:internal|node|v8)\/)/.test(str))
-);
-
+const builtins = Module.builtinModules;
 const JS_EXTENSIONS = new Set(['.js', '.mjs']);
 
-export function resolve(specifier, parentModuleURL /*, defaultResolve */) {
+const baseURL = new URL('file://');
+baseURL.pathname = `${process.cwd()}/`;
 
-  if (builtins.has(specifier)) {
+export function resolve(specifier, parentModuleURL = baseURL, defaultResolve) {
+  if (builtins.includes(specifier)) {
     return {
       url: specifier,
       format: 'builtin'
     };
   }
-
   if (/^\.{0,2}[/]/.test(specifier) !== true && !specifier.startsWith('file:')) {
     // For node_modules support:
     // return defaultResolve(specifier, parentModuleURL);
     throw new Error(
       `imports must begin with '/', './', or '../'; '${specifier}' does not`);
   }
-  const resolved = new url.URL(specifier, parentModuleURL);
+  const resolved = new URL(specifier, parentModuleURL);
   const ext = path.extname(resolved.pathname);
   if (!JS_EXTENSIONS.has(ext)) {
     throw new Error(
       `Cannot load file with non-JavaScript file extension ${ext}.`);
   }
-
   return {
     url: resolved.href,
     format: 'esm'
