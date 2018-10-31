13dc3678aa7c7e09d9ee40de983c469e6890230b
sengxiong91 <sengcheng.xiong@gmail.com>
2018-10-31 13:14:08 -0500

    Update models/botcase.js

--- models/botcase.js
+++ models/botcase.js
@@ -55,7 +55,132 @@ module.exports = {
 		};
 		callback(content);
 	},
-
+	gabumon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://vignette.wikia.nocookie.net/universosteven/images/f/fd/Gabumon.png/revision/latest?cb=20161113145227&path-prefix=es"
+			}
+		};
+		callback(content);
+	},
+	garuromon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://vignette.wikia.nocookie.net/digimon-adventure5140/images/4/49/Garurumon_tri.png/revision/latest?cb=20170913165523"
+			}
+		};
+		callback(content);
+	},
+	tentomon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://wikimon.net/images/thumb/3/34/Tentomon_adventure_fes_2017.png/180px-Tentomon_adventure_fes_2017.png"
+			}
+		};
+		callback(content);
+	},
+	kabuterimon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://4.bp.blogspot.com/-M9mN9V8akIM/UVNUKm8SOOI/AAAAAAAAO8w/G2-OH3PUCXA/s1600/kabuterimon.png"
+			}
+		};
+		callback(content);
+	},
+	gomamon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://vignette.wikia.nocookie.net/beastwarstransformers/images/d/da/Gomamon_Tri.png/revision/latest?cb=20170903103833"
+			}
+		};
+		callback(content);
+	},
+	ikkakumon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "vhttps://static.comicvine.com/uploads/original/11133/111335576/6488261-ikkakumon_by_million_mons_project-dbacmew.png"
+			}
+		};
+		callback(content);
+	},
+	patamon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://pre00.deviantart.net/f56a/th/pre/f/2016/267/c/f/patamon_by_cristian_kali-daiqswv.png"
+			}
+		};
+		callback(content);
+	},
+	angemon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://banner2.kisspng.com/20180406/hxw/kisspng-angemon-gatomon-patamon-digimon-adventure-tri-priest-5ac7b57511f7f4.9753060515230375570736.jpg"
+			}
+		};
+		callback(content);
+	},
+	biyomon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://orig00.deviantart.net/dfa1/f/2014/303/1/b/my_first_vector_of__biyomon__by_flutterflyraptor-d84qr1s.png"
+			}
+		};
+		callback(content);
+	},
+	birdramon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://vignette.wikia.nocookie.net/p__/images/8/8a/Birdramon_tri.png/revision/latest?cb=20170317060410&path-prefix=protagonist"
+			}
+		};
+		callback(content);
+	},
+	palmon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://banner2.kisspng.com/20180424/wqw/kisspng-palmon-digimon-digivolution-mimi-tachikawa-gomamon-green-plant-5adf09f6971b55.6272237115245665186189.jpg"
+			}
+		};
+		callback(content);
+	},
+	togemon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://banner2.kisspng.com/20180426/ikq/kisspng-togemon-digimon-adventure-tri-cactaceae-clip-art-harajuku-style-5ae1ef035d9f81.0243137515247562273835.jpg"
+			}
+		};
+		callback(content);
+	},
+	gatomon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://i.pinimg.com/originals/bc/0b/01/bc0b014795e61bc44ef94cf98c251423.jpg"
+			}
+		};
+		callback(content);
+	},
+	angewonmon: function(user, userID, channelID, args, callback){
+		var content = {
+			"color": 12345678,
+			"image": {
+				"url": "https://banner2.kisspng.com/20180409/tsq/kisspng-gatomon-angemon-digimon-angewomon-kari-kamiya-digimon-5acaf5b0cd3e30.5224375915232506088407.jpg"
+			}
+		};
+		callback(content);
+	},
 	// show user info for future use in database
 	myinfo: function(user, userID, channelID, args, callback){
 		var content = {
