7328e0fb0ea8cad4952975f77eacbf0c77ba75ea
zozyo <zzy.touhou@gmail.com>
2018-11-12 17:42:26 -0600

    Update models/random_gen.js

--- models/random_gen.js
+++ models/random_gen.js
@@ -4,7 +4,7 @@ var rule = new schedule.RecurrenceRule();
 
 exports.setTime = function (userID, args, callback){
 	console.log(args[0]);
-	rule.minute = parseInt(args[0]);
+	rule.second = parseInt(args[0]);
 	schedule.scheduleJob(rule, function(){
   		callback('The answer to life, the universe, and everything!');
 	});
